8d076a38ac91a34d201cc632d4489e1eb2a9fe89
Patrick W. Healy <phealy@phealy.com>
2026-08-18 18:52:33 -0500

    Rename module and add missing files

--- src/gadget/prog0.py
+++ src/gadget/prog0.py
@@ -1,10 +1,11 @@
 # LEGO slot:0 autostart
 
 from gadget import *
+from hub import port
 import runloop
 
 async def main():
-    await setup(wheel_diameter="small", turning_multiplier=1.432)
+    await setup(wheel_diameter="small", turning_multiplier=1.432, left_motor=port.A, right_motor=port.E)
     await gyro_move(distance=20, description="Model 1")
     await gyro_move(angle=-90)
     await gyro_move(angle=180)
