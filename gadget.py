bc6bba295c858c42351112c2fb7e889dd3b4d847
Patrick W. Healy <phealy@phealy.com>
2026-08-20 19:47:07 -0500

    implemented drive_until and turn_until

--- gadget.py
+++ gadget.py
@@ -4,20 +4,26 @@
 from hub import motion_sensor as ms, port as p, light_matrix as lm
 from math import pi, copysign
 from time import ticks_ms
+import color_sensor as cs, color as c
 import motor_pair as mp, motor as m
 import runloop
 
+COLOR_NAMES = (
+    "black", "magenta", "purple", "blue", "azure", "turquoise",
+    "green", "yellow", "orange", "red", "white",
+)
 START_TIME = ticks_ms()
 
 async def setup(
-    default_drive_velocity: int = 600,
-    default_turn_velocity: int = 600,
+    default_drive_velocity: int = 320,
+    default_turn_velocity: int = 200,
     default_acceleration: int = 1000,
     default_deceleration: int = 1000,
     default_stop_type: m.StopType = m.SMART_BRAKE,
     steering_correction: float = 1,
     left_motor: m.Motor = p.B,
     right_motor: m.Motor = p.A,
+    color_sensor: p.Port | None = None,
     drive_pair: mp.MotorPair = mp.PAIR_1,
     wheel_diameter: str | float = "small",
     turn_factor: float = 1,
@@ -52,7 +58,7 @@ async def setup(
 
     global DEFAULT_DRIVE_VELOCITY, DEFAULT_TURN_VELOCITY, DEFAULT_ACCELERATION
     global DEFAULT_DECELERATION, DEFAULT_STOP_TYPE, STEERING_CORRECTION
-    global DRIVE_MOTORS, DRIVE_PAIR, DEGREES_PER_CM, TURN_FACTOR
+    global DRIVE_MOTORS, DRIVE_PAIR, DEGREES_PER_CM, TURN_FACTOR, COLOR_SENSOR
 
     await runloop.until(ms.stable)
     DEFAULT_DRIVE_VELOCITY = default_drive_velocity
@@ -63,6 +69,7 @@ async def setup(
     STEERING_CORRECTION = steering_correction
     DRIVE_MOTORS = (left_motor, right_motor)
     DRIVE_PAIR = drive_pair
+    COLOR_SENSOR = color_sensor
     if wheel_diameter == "small":
         DEGREES_PER_CM = 360 / (5.6 * pi)
     elif wheel_diameter == "large":
@@ -101,7 +108,7 @@ def avg_relative_drive_distance_cm() -> float:
     return sum(distances) / len(distances) / DEGREES_PER_CM
 
 
-def angle_difference(angle: float, long_turn: bool) -> float:
+def angle_difference(angle: float, long_turn: bool = False) -> float:
     '''
     Calculate the difference between the current yaw angle and the desired angle. Wrap to -180 to 180.
         
@@ -115,7 +122,7 @@ def angle_difference(angle: float, long_turn: bool) -> float:
     return result
 
 
-def steering(angle: float, long_turn: bool) -> int:
+def steering(angle: float, long_turn: bool = False) -> int:
     '''Calculates the steering for move_for_degrees() based on the STEERING_CORRECTION
 
     Steering is based on how far off we are from the correct angle but adjusted to be no more than 50.
@@ -139,6 +146,8 @@ def log(message: str):
 
 async def gyro_move(distance: float = 0,
                     angle: float | None = None,
+                    drive_until: list[c.Color] | None = None,
+                    turn_until: list[c.Color] | None = None,
                     long_turn: bool = False,
                     drive_velocity: int | None = None,
                     drive_acceleration: int | None = None,
@@ -155,15 +164,17 @@ async def gyro_move(distance: float = 0,
     the specified angle, then drive and maintain the angle even if bumped off course.
     
     Args:
-        distance (float): The distance to drive in centimeters. May be omitted or 0 to just turn. Negative values back up.
-        angle (float): The angle to drive at. If not supplied, the current yaw angle will be used.
-        long_turn (bool): The default behavior is to turn the shortest direction possible to reach the heading. If long_turn is True, turn the longer direction.
-        drive_velocity (int): The velocity used for driving in degrees/second (100-1050).
-        drive_acceleration (int): The acceleration value used for driving in degrees/second^2 (100-10000)
-        turn_velocity (int): The velocity used for turning in degrees/second (100-1050).
-        turn_acceleration (int): The acceleration value used for turning in degrees/second^2 (100-10000)
-        turn_acceleration (int): The deceleration value used for turning in degrees/second^2 (100-10000)
-        description (str): The description printed in the status console output.
+        distance: The distance to drive in centimeters. May be omitted or 0 to just turn. Negative values back up.
+        angle: The angle to turn to and/or drive at. If not supplied, the current yaw angle will be used.
+        drive_until: A list[] of colors from the color module. The robot will drive until it has seen those colors in sequence.
+        turn_until: A list[] of colors from the color module. The robot will turn until it has seen those colors in sequence. When using turn_until, angle determines the maximum amount of rotation and the direction.
+        long_turn: The default behavior is to turn the shortest direction possible to reach the heading. If long_turn is True, turn the longer direction.
+        drive_velocity: The velocity used for driving in degrees/second (100-1050).
+        drive_acceleration: The acceleration value used for driving in degrees/second^2 (100-10000)
+        turn_velocity: The velocity used for turning in degrees/second (100-1050).
+        turn_acceleration: The acceleration value used for turning in degrees/second^2 (100-10000)
+        turn_acceleration: The deceleration value used for turning in degrees/second^2 (100-10000)
+        description: The description printed in the status console output.
     '''
     try:
         assert DRIVE_PAIR is not None
@@ -184,6 +195,10 @@ async def gyro_move(distance: float = 0,
     assert 100 <= drive_acceleration <= 10000, "acceleration must be between 100 and 10000"
     assert 100 <= turn_acceleration <= 10000, "deceleration must be between 100 and 10000"
     assert -180 <= angle <= 180, "angle must be between -180 and 180 degrees"
+    if drive_until is not None or turn_until is not None:
+        assert COLOR_SENSOR is not None, "until specified but color sensor not initialized"
+    if turn_until is not None:
+        assert angle_supplied, "angle must be supplied when using turn_until"
 
     # Internal functions
     async def print_status(status: str):
@@ -201,8 +216,6 @@ async def gyro_move(distance: float = 0,
                 f'| {status:<10}',
                 f'| Distance {distance:>6}',
                 f'| Desired yaw {angle:>5}',
-                f'| Velocity {velocity:>4}',
-                f'| Accel {acceleration:>4}',
                 f'| Yaw {round(yaw_angle(), 1):>6}',
                 f'| ΔYaw {round(angle_difference(angle, long_turn), 1):>6}',
                 f'| Driven {round(avg_relative_drive_distance_cm(), 1):>6}',
@@ -217,25 +230,48 @@ async def gyro_move(distance: float = 0,
     distance = abs(distance)
     drive_velocity = drive_velocity * direction
 
-    if angle_supplied:
-        (velocity, acceleration) = (turn_acceleration, turn_acceleration)
-        reset_relative_drive_distance()
-        await print_status("turning")
+    # Not doing color matching
+    if drive_until is None and turn_until is None:
+        if angle_supplied:
+            # Do a gyro tank turn
+            log("TODO: implement move turn instead of move_for_degrees")
+            await print_status("turning")
+            await mp.move_for_degrees(DRIVE_PAIR, int(angle_difference(angle, long_turn) * TURN_FACTOR), 100, stop = DEFAULT_STOP_TYPE, velocity=turn_velocity, acceleration=turn_acceleration, deceleration=turn_deceleration)
 
-        # Do a gyro tank turn
-        await mp.move_for_degrees(DRIVE_PAIR, int(angle_difference(angle, long_turn) * TURN_FACTOR), 100, stop = DEFAULT_STOP_TYPE, velocity=turn_velocity, acceleration=turn_acceleration, deceleration=turn_deceleration)
-
-    if distance is not None:
-        (velocity, acceleration) = (drive_acceleration, drive_acceleration)
-        await print_status("driving")
-
-        # Start driving, continuously correcting in a loop. Break out when we reach our distance.
-        reset_relative_drive_distance()
-        try:
+        if distance is not None:
+            # Start driving, continuously correcting in a loop. Break out when we reach our distance.
+            reset_relative_drive_distance()
+            await print_status("driving")
             while remaining_degrees_drive() > 0:
                 mp.move(DRIVE_PAIR, steering(angle, False) * direction, velocity=drive_velocity, acceleration=drive_acceleration)
                 await runloop.sleep_ms(20)
-        finally:
             mp.stop(DRIVE_PAIR, stop=DEFAULT_STOP_TYPE)
+
+    # Turn color matching
+    elif turn_until is not None:
+        log(f'gyro_move | {description:<12} | colorturn  |                | remaining {[COLOR_NAMES[color] for color in turn_until]}')
+        mp.move(DRIVE_PAIR, int(copysign(100, angle_difference(angle))), velocity=turn_velocity, acceleration=turn_acceleration)
+        # Keep turning while there are colors left to match and we haven't hit our max angle
+        while len(turn_until) > 0 and abs(angle_difference(angle)) > 1:
+            # If the current color sensor value matches the one at the front of the list, log and remove it.
+            if cs.color(COLOR_SENSOR) == turn_until[0]: # type: ignore
+                log(f'gyro_move | {description:<12} | colorturn  | saw {COLOR_NAMES[turn_until[0]]:<11} | remaining {[COLOR_NAMES[color] for color in turn_until[1:]]}')
+                turn_until.pop(0)
+            await runloop.sleep_ms(10)
+        log(f'gyro_move | {description:<12} | colorturn  | sequence complete')
+        mp.stop(DRIVE_PAIR, stop=DEFAULT_STOP_TYPE)
+
+    # Drive color matching
+    elif drive_until is not None:
+        # Start driving, continuously correcting in a loop. Break out when we reach our distance.
+        log(f'gyro_move | {description:<12} | colordrive |                 | remaining {[COLOR_NAMES[color] for color in drive_until]}')
+        reset_relative_drive_distance()
+        while len(drive_until) > 0:
+            mp.move(DRIVE_PAIR, steering(angle, False) * direction, velocity=drive_velocity, acceleration=drive_acceleration)
+            if cs.color(COLOR_SENSOR) == drive_until[0]: # type: ignore
+                log(f'gyro_move | {description:<12} | colordrive | saw {COLOR_NAMES[drive_until[0]]:<11} | remaining {[COLOR_NAMES[color] for color in drive_until[1:]]}')
+                drive_until.pop(0)
+            await runloop.sleep_ms(10)
+        mp.stop(DRIVE_PAIR, stop=DEFAULT_STOP_TYPE)
 
     await print_status("finish")
--- prog0.py
+++ prog0.py
@@ -2,19 +2,21 @@
 # Copyright (c) 2026 Patrick W. Healy <phealy@phealy.com>
 # SPDX-License-Identifier: MIT
 
-from gadget import *
 from hub import port
+from color_sensor import color
+from color import BLACK, WHITE, GREEN
 import runloop
 
 async def main():
-    await setup(wheel_diameter="small", turn_factor=1.447, left_motor=port.A, right_motor=port.B)
-    await gyro_move(angle = 90, long_turn=True)
-    await gyro_move(angle = 0, long_turn=True)
-    await gyro_move(angle = 90, long_turn=False)
-    await gyro_move(angle = 0, long_turn=False)
-    await gyro_move(distance = 20)
-    await gyro_move(distance = -20)
-        
+    await setup(wheel_diameter="small", turn_factor=1.447, left_motor=port.A, right_motor=port.B, color_sensor=port.C)
+    #await gyro_move(distance=-20, drive_velocity=200, description='backing up')
+    # await gyro_move(angle=90, long_turn=True, description='face east')
+    # await gyro_move(angle=0, description='face north')
+    #await gyro_move(drive_until=[GREEN, WHITE, BLACK], drive_velocity=200, description='driveblack')
+    await gyro_move(angle=-85)
+    await gyro_move(angle=20, turn_until=[WHITE, BLACK], turn_velocity=100)
+    await gyro_move(angle=0)
     log("completed")
 
+from gadget import *
 runloop.run(main())
